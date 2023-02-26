7fb42ddb14cbfd4e0dd34d2cc32fb0515e4c9365
Divyansh Sharma <92170111+Dev4821@users.noreply.github.com>
2023-02-26 23:29:19 +0530

    Create edit-distance.java

---
+++ edit-distance.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        char str1[]=word1.toCharArray();
+        char str2[]=word2.toCharArray();
+        int dp[][]=new int[str1.length+1][str2.length+1];
+        for(int i=0;i<dp.length;i++)
+        {
+            dp[i][0]=i;
+        }
+        for(int i=0;i<dp[0].length;i++)
+        {
+            dp[0][i]=i;
+        }
+        for(int i=1;i<str1.length+1;i++)
+        {
+            for(int j=1;j<str2.length+1;j++)
+            {
+                if(str1[i-1]==str2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=mini(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1;
+                }
+                
+            }
+        }
+        return dp[str1.length][str2.length];
+        
+    }
+    private int mini(int a,int b,int c)
+    {
+        int l= Math.min(a,b);
+        return Math.min(l,c);
+         
+        
+           
+        
+    }
+}
